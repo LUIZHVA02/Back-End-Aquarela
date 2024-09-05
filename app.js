dad9a44e6489e5d55b180e40ac37faac288237fc
Pedro Herique <phpedrinh64@gmail.com>
2024-09-05 09:35:01 -0300

    feat: "creating app.js"

---
+++ app.js
@@ -0,0 +1,11 @@
+/****************************************************************************************************************************************************
+* Objetivo: Cria uma API para respoder os dados de cadastro
+* Data: 05/09/2024
+* Autor: Luiz Vidal, Luan Oliveira, Pedro Barbosa, Ryan Alves & Vitória Azevedo
+* Versão: 1.0
+****************************************************************************************************************************************************/
+
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
